890e8afc90317a798e87cd953fe11009999ba672
lcxydemo <1015141113@qq.com>
2019-08-12 20:07:40 +0800

    add prototype pattern annotation

--- src/main/java/com/demik/designparttens/createpatterns/prototype/Test.java
+++ src/main/java/com/demik/designparttens/createpatterns/prototype/Test.java
@@ -4,7 +4,11 @@ import com.demik.designparttens.createpatterns.prototype.shape.Shape;
 import com.demik.designparttens.createpatterns.prototype.shape.ShapeFactory;
 
 /**
- * Function:
+ * Function:原型模式test
+ *
+ * 感觉其实就像是把比较难从数据库查的对象放到缓存里，下次查的时候直接从缓存里拿出来用了
+ *
+ * 原型模式比较常搭配的是工厂模式。一般都与工厂模式搭配使用
  *
  * @author liubing
  * Date: 2019/8/12 10:55 AM
